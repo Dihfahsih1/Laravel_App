7b560a129e30059a3b282cc777a83894683952e9
Mugoya Dihfahsih <dihfahsihm@gmail.com>
2018-12-18 14:52:05 +0300

    Post update, Edit And Deleting
    Simple Laravel Crud Operations

---
+++ resources/views/delete.blade.php
@@ -0,0 +1,37 @@
+      
+@include('inc.header')
+<div class="container">
+    <div class="row">
+        <div class="col-md-6">
+           <form action="{{ url('/edit', array($articles->id)) }}" method="post" class="form-horizontal">
+           	{{ csrf_field() }}
+            <fieldset>
+                 <legend>Laravel Application</legend>
+                          @if(count($errors)>0)
+                             @foreach($errors -> all() as $error)
+                               <div class=" alert alert-danger">
+                                   {{ $error }}
+                               </div>
+                             @endforeach                         
+                              @endif
+                            <div class="form-group">
+                                <label >Title</label>
+                                <input type="text" class="form-control" value="<?php echo $articles->title?>" placeholder=" type here title" name="title">
+                            </div>
+                            <div class="form-group">
+                                <label>Description</label>
+                                <textarea  name="description" class="form-control"  placeholder="type here description" ><?php echo $articles->description?></textarea>
+                            </div>
+                            <div class="form-group">
+                             <div class="col-lg-10 col-lg-offset-2"> 	
+	                            <button type="submit" name="save" class="btn btn-primary">Update</button>
+	                            
+	                            <a href="{{ url('/') }}" class="btn btn-primary">Back</a>
+	                        </div>
+	                       </div>     
+             </fieldset>
+           </form>  
+        </div>
+    </div>
+</div>
+@include('inc.footer')
--- resources/views/update.blade.php
+++ resources/views/update.blade.php
@@ -23,7 +23,7 @@
                                 <textarea  name="description" class="form-control"  placeholder="type here description" ><?php echo $articles->description?></textarea>
                             </div>
                             <div class="form-group">
-                             <div class="col-lg-10 col-lg-offset-2"> 	
+                             <div class="col-lg-10 col-lg-offset-2" > 	
 	                            <button type="submit" name="save" class="btn btn-primary">Update</button>
 	                            
 	                            <a href="{{ url('/') }}" class="btn btn-primary">Back</a>
